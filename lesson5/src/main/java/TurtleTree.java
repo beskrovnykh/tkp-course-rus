f7ebad96e451a93e6c3ccbe57ee54af5531e9da3
Andrey Beskrovnykh <andrey.beskrovnykh@yandex.ru>
2017-11-18 16:36:52 +0700

    Translate TurtleTree

--- lesson5/src/main/java/TurtleTree.java
+++ lesson5/src/main/java/TurtleTree.java
@@ -4,47 +4,57 @@ import java.util.HashMap;
 
 import org.teachingextensions.logo.Tortoise;
 
+/**
+ *      Задание:
+ *          Заменить комментарии ниже инструкциями на языке Java
+ *
+ *      Словарь:
+ *          Branch - ветвь
+ *          Adjust - регулировать
+ *          Shorter - более короткая
+ *          Lower - нижняя
+ **/
 public class TurtleTree {
   @SuppressWarnings("unused")
   public static void main(String[] args) {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#10
-    //    Turn the background black  --#21
-    //    The current branch length = 60 --#1.2
-    //    drawBranch(recipe below) --#2.1
+    //    Сделать так, чтобы черепашка передвигалась как можно быстрее --#10
+    //    Сделать фон окна черным  --#21
+    //    Задать значение переменной length = 60 --#1.2
+    //    Вызвать метод drawBranch(recipe below) --#2.1
     //
-    //    ------------- Recipe for drawBranch --#2.2
-    //        If the current branch length is greater than zero, do the rest of this recipe --#5
-    //        adjustColor (recipe below)--#15.1
-    //        ------------- Recipe for adjustColor --#15.2
+    //    ------------- Метод drawBranch --#2.2
+    //        Если текущее значение переменной length больше чем ноль, выполнить следующие действия (вплоть до окончания метода) --#5
+    //        Вызвать метод adjustColor (см. ниже) --#15.1
+    //        ------------- Метод adjustColor --#15.2
     HashMap<Integer, Color> colors = new HashMap<Integer, Color>();
-    //            A 10 pixel long branch is lime --#20
-    //            A 20 pixel long branch is forest green --#19
-    //            A 30 pixel long branch is dark green --#18
-    //            A 40 pixel long branch is olive --#17
-    //            A 50 pixel long branch is sienna --#14
-    //            A 60 pixel long branch is saddle brown (TIP: Put the values into the 'colors' HashMap)--#13 
-    //            Get the value of the branch length from the 'colors' HashMap and use that to set the pen color --#21
-    //        ------------- End of adjustColor --#15.3
+    //            Ветвь длиной 10 имеет цвет lime --#20
+    //            Ветвь длиной 20 имеет цвет forest green --#19
+    //            Ветвь длиной 30 имеет цвет dark green --#18
+    //            Ветвь длиной 40 имеет цвет olive --#17
+    //            Ветвь длиной 50 имеет цвет sienna --#14
+    //            Ветвь длиной 60 имеет цвет saddle brown (TIP: сохранить цвета в хэш-таблице 'colors')--#13 
+    //            Использовать значение переменной length из хэш-таблицы 'colors', для того чтобы задать перу нужный цвет --#21
+    //        ------------- Конец метода adjustColor --#15.3
     //
-    //        Move the tortoise the length of the current branch --#1.1
-    //        drawLowerBranches (recipe below) --#6.1
+    //        Переместить черепашку на значение переменной length --#1.1
+    //        Вызвать метод drawLowerBranches (см. ниже) --#6.1
     //
-    //        ------------- Recipe for drawLowerBranches --#6.2
-    //            Turn the Tortoise 30 degrees to the right --#3
-    //            drawShorterBranch (recipe below) --#8.1
+    //        ------------- Метод drawLowerBranches --#6.2
+    //            Повернуть черепашку на угол, равный 30 градусов по часовой стрелке --#3
+    //            Вызвать метод drawShorterBranch (см. ниже) --#8.1
     //
-    //            ------------- Recipe for drawShorterBranch --#8.2
-    //                drawBranch (10 pixels shorter) --#4
-    //            ------------- End of drawShorterBranch recipe --#8.3
+    //            ------------- Метод drawShorterBranch --#8.2
+    //            Вызвать метод drawBranch (на 10 пикселей короче) --#4
+    //            ------------- Конец метода drawShorterBranch --#8.3
     //
-    //            Turn the Tortoise 60 degrees to the left --#7
-    //            drawShorterBranch --#9
-    //            Turn the Tortoise 30 degrees to the right --#12
-    //            adjustColor --#16
-    //            Move the tortoise backward the length of the current branch --#11
-    //        ------------- End of drawLowerBranches recipe --#6.3
+    //            Повернуть черепашку на 60 градусов против часовой стрелке --#7
+    //            Вызвать метод drawShorterBranch --#9
+    //            Повернуть черепашку на 30 градусов по часовой стрелке --#12
+    //            Вызвать метод adjustColor --#16
+    //            Переместить черепашку на значение переменной length в обратном направлении --#11
+    //        ------------- Конец метода drawLowerBranches --#6.3
     //
-		// ------------- End of drawBranch recipe --#2.3
+	// ------------- Конец метода drawBranch --#2.3
 	}
 }
